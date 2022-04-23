e9303382ddb824d65e27b157f71b4ef590a93ce4
Dbrys <brysdillon@gmail.com>
2022-04-22 20:02:22 -0400

    Added antd and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
+import styled from 'styled-components';
+
+const colors = ['red', 'yellow', 'blue', 'green'];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonContainer>
+        {colors.map((color, i) => {
+          return (
+            <Button key={i} type="primary" style={{ backgroundColor: color }} />
+          );
+        })}
+      </ButtonContainer>
+      <Button type="primary">Start</Button>
     </div>
   );
 }
+
+const ButtonContainer = styled.div`
+  max-width: 250px;
+  text-align: center;
+  .ant-btn-primary {
+    cursor: pointer;
+    padding: 50px 40px;
+    margin: 10px;
+    border-radius: 20%;
+    :hover {
+      opacity: 0.7;
+      outline: 1px solid black;
+    }
+  }
+`;
 
 export default App;
