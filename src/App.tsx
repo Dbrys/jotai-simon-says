36edcafb6513806849645944776c67a9e65044bf
Dbrys <brysdillon@gmail.com>
2022-04-23 13:58:15 -0400

    Style update and inital use of jotai atom and useAtom

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import { Button } from 'antd';
+import { useAtom, atom } from 'jotai';
 import './App.css';
 import styled from 'styled-components';
 
 const colors = ['red', 'yellow', 'blue', 'green'];
 
+const disabled = atom(false);
+
 function App() {
+  const [isDisabled, setIsDisabled] = useAtom(disabled);
+  const handleStartClick = () => {
+    setIsDisabled(true);
+  };
   return (
     <div className="App">
       <ButtonContainer>
         {colors.map((color, i) => {
           return (
-            <Button key={i} type="primary" style={{ backgroundColor: color }} />
+            <Button
+              key={i}
+              type="primary"
+              style={{ backgroundColor: color }}
+            ></Button>
           );
         })}
       </ButtonContainer>
-      <Button type="primary">Start</Button>
+      <Button
+        disabled={isDisabled}
+        onClick={handleStartClick}
+        type="primary"
+        shape="round"
+        size="large"
+      >
+        Start
+      </Button>
     </div>
   );
 }
 
 const ButtonContainer = styled.div`
-  max-width: 250px;
+  max-width: 300px;
   text-align: center;
+  margin-bottom: 50px;
+  .ant-btn:empty {
+    visibility: visible;
+  }
   .ant-btn-primary {
     cursor: pointer;
     padding: 50px 40px;
